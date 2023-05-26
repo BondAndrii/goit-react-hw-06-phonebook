793c2d28d8e7bee2b0816446a11bedc293200cfa
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-05-26 12:03:39 +0300

    create new reducers by createReducer

--- src/redux/filter/reducer.js
+++ src/redux/filter/reducer.js
@@ -1,14 +1,24 @@
-
+import { createReducer } from "@reduxjs/toolkit";
+import { addFilter, doClear } from "./actions";
 
 const filterInitialState = '';
 
-export const filterReducer = (state = filterInitialState, action) => {
-     switch (action.type) {
-         case "filter/addFilter":
-             return action.payload;
-         case "filter/doClear":
-             return "";
-        default: 
-            return state;
+export const filterReducer = createReducer(filterInitialState, {
+    [addFilter]: (state, action) => {
+        return action.payload;
+    },
+    [doClear]: (state, action) => {
+        return ""
     }
-}
+})
+
+// export const filterReducer = (state = filterInitialState, action) => {
+//      switch (action.type) {
+//          case "filter/addFilter":
+//              return action.payload;
+//          case "filter/doClear":
+//              return "";
+//         default: 
+//             return state;
+//     }
+// }
